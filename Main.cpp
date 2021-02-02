f9cb5f164b6d5a37dd2de6ea2b98e5908a12a4f0
DESKTOP-715VRP9\Lelo <paul@edu.univali.br>
2021-02-02 10:30:47 -0300

    15 - Indexed Draws

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@
 const GLint WIDHT = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -60,15 +60,27 @@ void main()												\n\
 
 void CreateTriangle()
 {
+	unsigned int indices[] = { 
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat verticies[] =
 	{
 		  -1.0f, -1.0f, 0.0f,
+		   0.0f, -1.0f, 1.0f,
 		   1.0f, -1.0f, 0.0f,
 		   0.0f,  1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(verticies), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -78,6 +90,7 @@ void CreateTriangle()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glBindVertexArray(0);
 }
@@ -206,6 +219,7 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
 	glViewport(0, 0, bufferWidht, bufferHeight);
 
 	CreateTriangle();
@@ -253,11 +267,11 @@ int main()
 		}
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glm::mat4 model(1.0f);
 
-		//model = glm::rotate(model, toRadians * curAngle, glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.4, 0.4, 1.0f));
 		
@@ -268,7 +282,10 @@ int main()
 
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 		glBindVertexArray(0);
 
 		glUseProgram(0);
